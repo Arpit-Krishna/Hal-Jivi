37723f3c4ff207486a4547096bd745027c06fae5
Arpit Krishna <118879450+Arpit-Krishna@users.noreply.github.com>
2024-03-19 16:01:53 +0530

    lorem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <p>HAL JIVI</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel error, nulla dolore, facere perferendis id sit porro magni atque earum possimus quam reiciendis libero delectus ipsa eum maiores, sunt ullam!</p>
     </div>
   );
 }
