156db9ccfed6f233a567c01b7061fe670b834a80
Arpit Krishna <118879450+Arpit-Krishna@users.noreply.github.com>
2024-03-19 18:08:42 +0530

    update cancle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <p>HAL JIVI</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis illum dolores ullam corrupti nisi odio placeat ab doloribus necessitatibus autem, culpa, ducimus eos aperiam praesentium cumque atque doloremque corporis unde!</p>
     </div>
   );
 }
