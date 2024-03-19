38afa82c7cf2eccd9adc482b208cba83d99f65e4
222Aryan <aryantrivedi222@gmail.com>
2024-03-19 18:26:38 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ function App() {
     <div className="App">
       <p>HAL JIVI</p>
       <p>Aryan part</p>
+      <p>
+      JavaScript (JS) is a versatile programming language commonly used for web development. Its flexibility allows developers to create dynamic and interactive web pages.
+      </p>
 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis illum dolores ullam corrupti nisi odio placeat ab doloribus necessitatibus autem, culpa, ducimus eos aperiam praesentium cumque atque doloremque corporis unde!</p>
 
