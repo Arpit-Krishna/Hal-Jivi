e1bce5fe5f836d27715cb1781b42311a2cb48781
avantikaBajpai20 <119681103+avantikaBajpai20@users.noreply.github.com>
2024-03-19 18:01:21 +0530

    uodate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <p>HAL JIVI</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis illum dolores ullam corrupti nisi odio placeat ab doloribus necessitatibus autem, culpa, ducimus eos aperiam praesentium cumque atque doloremque corporis unde!</p>
     </div>
   );
 }
