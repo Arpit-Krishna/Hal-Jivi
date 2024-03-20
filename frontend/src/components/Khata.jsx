4ff0d905df30d3c853fd761a7a71d5a1df68bdb7
Arpit Krishna <118879450+Arpit-Krishna@users.noreply.github.com>
2024-03-20 16:54:47 +0530

    .

--- frontend/src/components/Khata.jsx
+++ frontend/src/components/Khata.jsx
@@ -3,7 +3,7 @@ import './Khata.css'
 
 export default function Khata() {
   return (
-    <div className="fixed bottom-0 right-0 m-4">
+    <div className="fixed bottom-0 right-0 m-8">
         <div className="flex items-center justify-center h-20 w-20 bg-[#415041] rounded-[50%]">
         <a className="button" href="/khata" alt="Add to Khata"> <h1>+</h1> </a>
         </div>
