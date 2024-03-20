428f8d979a0e631d6d11ba43db50ef619d62cc02
Arpit Krishna <118879450+Arpit-Krishna@users.noreply.github.com>
2024-03-20 16:15:14 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <p>HAL JIVI</p>
-      <p>Aryan part</p>
-      <p>
-      JavaScript (JS) is a versatile programming language commonly used for web development. Its flexibility allows developers to create dynamic and interactive web pages.
-      </p>
-
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis illum dolores ullam corrupti nisi odio placeat ab doloribus necessitatibus autem, culpa, ducimus eos aperiam praesentium cumque atque doloremque corporis unde!</p>
-
+      <Header/>
     </div>
   );
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+import logo from './assets/logo.png';
+import user from './assets/user.png';
+
+const Header = () => {
+  return (
+    <div className="Navbar flex justify-between mx-2 my-1 rounded-3xl">
+      <div className="flex items-center">
+          <div className="">
+            <img src={logo} className="h-10 mx-5 my-1" alt="Hal-Jivi"/>
+          </div>
+          <div className="">
+            <a className="mx-1" href="/">Home</a>
+            <a className="mx-1" href="/about">About</a>
+            <a className="mx-1" href="/customercare">Customer Care</a>
+          </div>
+      </div>
+      <div className="mx-5 flex items-center">
+      <img src={user} className="h-8 mx-1.5 my-1" alt="user"/>
+        <p className="">User</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
